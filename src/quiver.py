8e7f5377e57a098eb2cf5cbc38f8af8312fc3c7c
Thayer Anderson <tanderson11@gmail.com>
2016-08-01 19:27:24 -0400

    Calculation of orthonormal basis.

--- src/quiver.py
+++ src/quiver.py
@@ -11,16 +11,24 @@ def _g09_triangle_serial(row,col):
     triangle = lambda n: n*(n+1)/2
     return triangle(row) + col
 
+class IsotopolgueFactory(object):
+    pass
+
 class Isotopologue(object):
     def __init__(self, system, masses):
-        self.iitensor = self.calculate_inertia_tensor(masses)
+        self.system = system
+        self.masses = masses
+        self.number_of_atoms = system.number_of_atoms
+        self.rcm, self.iitensor = self.calculate_inertia_tensor(masses, system.positions)
         self.mw_hessian = self.calculate_mw_hessian(masses)
+
+        self.calculate_internal_hessian(masses)
     
-    def calculate_inertia_tensor(self, masses):
-        # calculate cartesian center of mass to find intertia tensor relative to com
+    def calculate_inertia_tensor(self, masses, positions):
+        # calculate cartesian center of mass to find intertia tensor relative to center of mass
         rcm = np.zeros(3)
         total_mass = 0
-        for i in xrange(0, number_of_atoms):
+        for i in xrange(0, self.number_of_atoms):
             for e in xrange(0,3):
                 total_mass += masses[i]
                 rcm[e] += positions[i][e] * masses[i]
@@ -31,18 +39,80 @@ class Isotopologue(object):
         iitensor = np.zeros(shape=(3,3))
         for e1 in xrange(0,3):
             for e2 in xrange(0,3):
-                for i in xrange(0, number_of_atoms):
+                for i in xrange(0, self.number_of_atoms):
                     if e1 == e2:
                         iitensor[e1,e2] += masses[i] * ((positions[i,(e1+1)%3]-rcm[(e1+1)%3])**2
                                                       + (positions[i,(e1+2)%3]-rcm[(e1+2)%3])**2)
                     else:
                         iitensor[e1,e2] += -1 * masses[i] * (positions[i,e1]-rcm[e1]) * (positions[i,e2]-rcm[e2])
 
-        return iitensor
+        return rcm, iitensor
 
-    def calculate_mw_hessian(self):
+    def calculate_frequencies(self):
         pass
-    
+
+    def calculate_mw_hessian(self, masses):
+        pass
+
+    def calculate_internal_hessian(self, masses):
+        vectors = []
+        for e in xrange(0,3):
+            v = np.zeros(3*self.number_of_atoms)
+            for i in xrange(0, self.number_of_atoms):
+                v[3*i:3*i+3] = np.array([1.0 if x == e else 0.0 for x in xrange(0,3)]) * masses[i]
+            vectors.append(v)
+        
+        # order concerns?
+        v,w = np.linalg.eig(self.iitensor)
+        
+        for e1 in xrange(0,3):
+            v = np.zeros(3*self.number_of_atoms)
+            for i in xrange(0, self.number_of_atoms):
+                p = np.zeros(3)
+                for e in xrange(0,3):
+                    p[e] = np.inner(w[e], self.system.positions[i])
+
+                for e2 in xrange(0,3):
+                    v[3*i+e2] = (p[(e2+1)%3] * w[e1][(e2+2)%3] - p[(e2+2)%3] * w[e1][(e2+1)%3])/np.sqrt(masses[i])
+            vectors.append(v)
+
+        def normalize(v):
+            norm=np.linalg.norm(v)
+            if norm==0: 
+               raise ValueError
+            return v/norm
+
+        normalized_vectors = []
+        zero_vectors = []
+        for v in vectors:
+            try:
+                normalized_vectors.append(normalize(v))
+            except ValueError:
+                zero_vectors.append(v)
+        
+        for u in normalized_vectors:
+            for v in normalized_vectors:
+                print np.inner(u,v)
+
+        def proj(u,v):
+            # project u onto v
+            #print np.inner(v,u)/np.inner(u,u)
+            return np.inner(v,u)/np.inner(u,u) * u
+
+        standard_basis = [np.array([1.0 if x == i else 0.0 for x in xrange(0,3*self.number_of_atoms)]) for i in xrange(0,3*self.number_of_atoms)]
+        while len(normalized_vectors) + len(zero_vectors) < 3*self.number_of_atoms:
+            test_vector = standard_basis.pop()
+            for v in normalized_vectors:
+                test_vector -= proj(test_vector, v)
+            try:
+                normalized_vectors.append(normalize(test_vector))
+            except ValueError:
+                pass
+
+        print normalized_vectors
+        for u in normalized_vectors:
+            for v in normalized_vectors:
+                print np.inner(u,v)
     def calculate_rpfr(self):
         pass
 
@@ -75,13 +145,15 @@ class System(object):
                     masses[center_number] = raw_geom_line[1]
                     for e in xrange(0,3):
                         positions[center_number][e] = raw_geom_line[3+e]
-                                                
-
+                
                 # units = hartrees/bohr^2 ?
                 hessian = self._parse_g09_hessian(out_data)
 
         #copy fields
         self.hessian = hessian
+        self.positions = positions
+        self.masses = masses
+        self.atomic_numbers = atomic_numbers
 
     def _parse_g09_hessian(self, data):
         #    number_of_atoms = 
@@ -98,5 +170,5 @@ class System(object):
                 fcm[i,j] = raw_fcm[_g09_triangle_serial(i,j)]
         return fcm
 
-gs = System("../test/claisen_gs.out")
-
+gs = System("../test/co2.out")
+gsiso = Isotopologue(gs, [1.0 for i in xrange(0, gs.number_of_atoms)])
