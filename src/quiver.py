ea48348cdb4209cd4db9f3fb84e7e073322721a8
Thayer Anderson <tanderson11@gmail.com>
2016-08-01 15:08:29 -0400

    Geometry parsing done up to mass constants.

--- src/quiver.py
+++ src/quiver.py
@@ -17,6 +17,7 @@ class System(object):
         with open(outfile, 'r') as f:
             out_data = f.read()
             if style == "g09":
+                # read in the number of atoms
                 m = re.search("NAtoms\= + ([0-9]+)", out_data)
                 if m:
                     number_of_atoms = int(m.group(1))
@@ -24,9 +25,19 @@ class System(object):
                     raise AttributeError("Number of atoms not detected.")
                 self.number_of_atoms = number_of_atoms
                 
-                #m = re.match("Forces \(Hartrees/Bohr\)(.+)Cartesian Forces")
-                for match in re.finditer("Forces \(Hartrees/Bohr\)(.+)Cartesian Forces", out_data, re.DOTALL):
-                    print match.group(0)
+                # read in the last geometry (assumed cartesian coordinates)
+                atomic_number = [0 for i in xrange(number_of_atoms)]
+                position = [None for i in xrange(number_of_atoms)]
+                mass = np.array([0.0 for i in xrange(number_of_atoms)])
+                for m in re.finditer("Standard orientation:(.+?)Rotational constants \(GHZ\)", out_data, re.DOTALL):
+                    pass
+                
+                for l in m.group(1).split('\n')[5:-2]:
+                    raw_geom_line = filter(None, l.split(' '))
+                    center_number = int(raw_geom_line[0]) - 1
+                    atomic_number[center_number] = int(raw_geom_line[1])
+                    mass[center_number] = raw_geom_line[1]
+                    position[center_number] = np.array(raw_geom_line[3:])
 
                 # units = hartrees/bohr^2 ?
                 hessian = self._parse_g09_hessian(out_data)
@@ -48,6 +59,9 @@ class System(object):
             for j in xrange(0, 3*self.number_of_atoms):
                 fcm[i,j] = raw_fcm[_g09_triangle_serial(i,j)]
         return fcm
+    
+    def calculate_rpfr(self):
+        pass
 
 
 gs = System("../test/claisen_gs.out")
